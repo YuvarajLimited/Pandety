531364b7dcd24a71c06bc35a6daaf8c317cffdfb
Yuvaraj Ramesh <ryuvaraj9820@gmail.com>
2022-09-01 12:21:53 +0530

    nothing

--- src/app/tabs/setting/aboutus/aboutus.module.ts
+++ src/app/tabs/setting/aboutus/aboutus.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { AboutusPageRoutingModule } from './aboutus-routing.module';
 
 import { AboutusPage } from './aboutus.page';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    AboutusPageRoutingModule
+    AboutusPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [AboutusPage]
 })
